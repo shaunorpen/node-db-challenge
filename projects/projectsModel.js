ea18789c85b4d07a47a6a6eed5b12e4d56f0ed76
shaunorpen <shaunorpen@users.noreply.github.com>
2019-11-08 10:17:02 +0000

    Add getProjectById

--- projects/projectsModel.js
+++ projects/projectsModel.js
@@ -12,6 +12,27 @@ module.exports = {
       })
     );
   },
+  getProjectById: project_id => {
+    const project = db("projects").where({ id: project_id });
+    const resources = db("resources as r")
+      .join("project_resources as pr", "r.id", "pr.resource_id")
+      .select("r.id", "r.name", "r.description")
+      .where({ "pr.project_id": project_id });
+    const tasks = db("tasks").where({ project_id: project_id });
+    return Promise.all([project, resources, tasks]).then(values => {
+      return {
+        ...values[0][0],
+        completed: helpers.convertBoolean(values[0][0].completed),
+        tasks: values[2].map(task => {
+          return {
+            ...task,
+            completed: helpers.convertBoolean(task.completed)
+          };
+        }),
+        resources: values[1]
+      };
+    });
+  },
   addProject: project => {
     return db("projects").insert(project);
   }
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const projects = require("./projectsModel");
-const helpers = require("../helpers/helpers");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   projects
     .getAllProjects()
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "Error retrieving projects: " + error.message });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  projects
+    .getProjectById(req.params.id)
     .then(data => {
       res.status(200).json(data);
     })
