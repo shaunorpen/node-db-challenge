cac4a431090364375ca8e96c46f88a2c24403c15
shaunorpen <shaunorpen@users.noreply.github.com>
2019-11-08 09:35:02 +0000

    Update task list to include project name and description

--- tasks/tasksModel.js
+++ tasks/tasksModel.js
@@ -2,7 +2,10 @@ const db = require("../data/dbConfig");
 
 module.exports = {
   getTasksForProject: project_id => {
-    return db("tasks").where({ project_id: project_id });
+    return db("tasks")
+      .join("projects", "tasks.project_id", "projects.id")
+      .select("tasks.*", "projects.name as project_name", "projects.description as project_description")
+      .where({ project_id: project_id });
   },
   addTaskToProject: task => {
     return db("tasks").insert(task);
